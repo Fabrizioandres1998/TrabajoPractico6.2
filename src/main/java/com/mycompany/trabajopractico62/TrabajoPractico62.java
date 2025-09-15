7f4908f36a8edf906450fda5e8ea6c83bf8140d3
Fabrizio D'Isidoro <fabrizioandres98@gmail.com>
2025-09-15 18:29:56 -0300

    primer commit agregar producto y verlo por tabla por categoria funciona

---
+++ src/main/java/com/mycompany/trabajopractico62/TrabajoPractico62.java
@@ -0,0 +1,12 @@
+package com.mycompany.trabajopractico62;
+
+import vista.DeTodoSA;
+
+public class TrabajoPractico62 {
+
+    public static void main(String[] args) {
+        DeTodoSA deTodo = new DeTodoSA();
+        deTodo.setVisible(true);
+        deTodo.setLocationRelativeTo(null);
+    }
+}
